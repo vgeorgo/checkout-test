c63ddce69d18fa481116ca7918996ec95b3a3ba6
Victor Georg Oliveira <vgeorgoliveira@gmail.com>
2021-08-22 15:16:11 -0300

    Success component

--- store/src/components/pages/Success.vue
+++ store/src/components/pages/Success.vue
@@ -1,5 +1,14 @@
 <template>
   <v-container>
+    <v-card color="green" class="mb-5">
+      <v-card-title>
+        <span class="text-h6 font-weight-light">Enjoy!</span>
+      </v-card-title>
+
+      <v-card-text class="text-h5 font-weight-bold">
+        Order successfully processed, have a nice day!
+      </v-card-text>
+    </v-card>
     <v-container class="d-flex flex-row-reverse">
       <v-btn color="yellow" @click="backStep" large> Back </v-btn>
     </v-container>
